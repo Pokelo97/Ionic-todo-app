a143624094e5fab321ba3020336d30b1eee350f7
lucky mohlala <lucky10bokalo@gmail.com>
2021-08-19 23:48:48 +0200

    added .env file

--- todo-app/src/firebase/firebase.js
+++ todo-app/src/firebase/firebase.js
@@ -3,13 +3,11 @@ import "firebase/auth";
 import "firebase/firestore"
 
 export const auth = firebase.initializeApp({
-  apiKey: "AIzaSyAKOIqr-HHHwtz2BY6yYkUuCGNZWoUFzUg",
-  authDomain: "todo-app-2b873.firebaseapp.com",
-  projectId: "todo-app-2b873",
-  storageBucket: "todo-app-2b873.appspot.com",
-  messagingSenderId: "943536017740",
-  appId: "1:943536017740:web:9ad25efd6993602fb8bfe9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }).auth();
-
-
 export const db = firebase.firestore();
